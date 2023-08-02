034da13b8e75c61e5e05915384d5c365a3d67ddd
Jade Thompson-Tavai <jhp4379@autuni.ac.nz>
2023-08-03 09:07:11 +1200

    added connectDB to app.js

--- week3/app.js
+++ week3/app.js
@@ -1,8 +1,12 @@
 //app.js
 
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//connect to DB
+connectDB();
 
 app.get('/', (req, res) => res.send('Hello, world!'));
 
