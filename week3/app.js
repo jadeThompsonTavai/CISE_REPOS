a71ef4709bacfc3befae0c83a85a53fc3d65ed18
Jade Thompson-Tavai <jhp4379@autuni.ac.nz>
2023-08-03 08:57:20 +1200

    Add: config folder and files to connect to MongoDB

--- week3/app.js
+++ week3/app.js
@@ -8,4 +8,4 @@ app.get('/', (req, res) => res.send('Hello, world!'));
 
 const port = process.env.PORT || 8082;
 
-app.listen(port, () => console.log('Server running on port ${port}'));
+app.listen(port, () => console.log('Server running on port' + port));
